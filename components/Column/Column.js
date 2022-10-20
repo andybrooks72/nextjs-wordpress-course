93417304b44bb7a3aa0b3b3b7f90db5823021186
andybrooks72 <92591249+andybrooks72@users.noreply.github.com>
2022-10-20 23:12:22 +0100

    fixed widthStyle in column

--- components/Column/Column.js
+++ components/Column/Column.js
@@ -3,6 +3,6 @@ export const Column = ({ children, width, textColor, backgroundColor }) => {
     const backgroundColorStyle = backgroundColor ? {backgroundColor} : {}
     const widthStyle = width ? {minWidth: width, flexGrow: 1} : {flexGrow: 1, flexBasis: 0}
     return (
-        <div style={{widthStyle, ...textColorStyle, ...backgroundColorStyle}} className="px-2 py-5">{children}</div>
+        <div style={{...widthStyle, ...textColorStyle, ...backgroundColorStyle}} className="px-2 py-5">{children}</div>
     )
 }
