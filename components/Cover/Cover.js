242b2cb002de3d97601389f8d1878b21ff63c966
andybrooks72 <92591249+andybrooks72@users.noreply.github.com>
2022-10-20 21:33:08 +0100

    fixed cover component

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -5,7 +5,7 @@ export const Cover = ({ children, background }) => {
     const { featuredImage } = usePageContext()
     return (
         <div className="h-screen text-white bg-slate-800 relative min-h-[400px] flex justify-center items-center">
-            {!!background || featuredImage && (
+            {(!!background || !!featuredImage) && (
                 <Image alt="Cover" src={background || featuredImage} layout="fill" objectFit="cover" className="mix-blend-soft-light" />
             )}
             <div className="max-w-5xl z-10">
